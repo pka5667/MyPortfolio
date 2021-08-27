9c64859e291c56e967505865374d372cc986fd6b
Pulkit Agarwal <pka5667@gmail.com>
2021-08-27 21:24:46 +0530

    commit

--- resumeProject/settings.py
+++ resumeProject/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # mannually added
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- resumeProject/urls.py
+++ resumeProject/urls.py
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from core import views
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
+    # for static files
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
+
     # path('admin/', admin.site.urls),
     path("", views.index, name="home"),
     path("contact", views.contact, name="contact"),

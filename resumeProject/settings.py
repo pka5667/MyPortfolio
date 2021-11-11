e4a48badc58820b3f29f104470f0d6b39610a3f8
Pulkit Kumar Agarwal <63954083+pka5667@users.noreply.github.com>
2021-11-11 10:23:56 +0530

    Update

--- resumeProject/settings.py
+++ resumeProject/settings.py
@@ -123,9 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collectedstatic')
 
-# mannually added
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
